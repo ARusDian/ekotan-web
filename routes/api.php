4f1c47e57637ecc93451b7ce110b33f73c701053
RUVNE <dian.galleon@gmail.com>
2023-11-17 00:43:53 +0800

    feat: middleware for api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PrizeController;
 use App\Http\Controllers\QuestController;
 use App\Http\Controllers\QuestCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,19 +21,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request)
 {
-    return $request->user();
+    return json_decode(Auth::user());
 });
 
-Route::get('article', [ArticleController::class, 'indexApi'])->name('api.article.index');
-Route::get('article/{article}', [ArticleController::class, 'showApi'])->name('api.article.show');
-Route::get('prize', [PrizeController::class, 'indexApi'])->name('api.prize.index');
-Route::get('prize/{prize}', [PrizeController::class, 'showApi'])->name('api.prize.show');
-Route::get('quest', [QuestController::class, 'indexApi'])->name('api.quest.index');
-Route::get('quest/{quest}', [QuestController::class, 'showApi'])->name('api.quest.show');
-Route::get('quest-category', [QuestCategoryController::class, 'index'])->name('api.quest-category.index');
-Route::get('quest-category/{questCategory}', [QuestCategoryController::class, 'show'])->name('api.quest-category.show');
+Route::post('login', [AuthController::class, 'getToken'])->name('api.login');
 
 Route::middleware('auth:sanctum')->group(function ()
 {
+    Route::get('article', [ArticleController::class, 'indexApi'])->name('api.article.index');
+    Route::get('article/{article}', [ArticleController::class, 'showApi'])->name('api.article.show');
+    Route::get('prize', [PrizeController::class, 'indexApi'])->name('api.prize.index');
+    Route::get('prize/{prize}', [PrizeController::class, 'showApi'])->name('api.prize.show');
+    Route::get('quest', [QuestController::class, 'indexApi'])->name('api.quest.index');
+    Route::get('quest/{quest}', [QuestController::class, 'showApi'])->name('api.quest.show');
+    Route::get('quest-category', [QuestCategoryController::class, 'index'])->name('api.quest-category.index');
+    Route::get('quest-category/{questCategory}', [QuestCategoryController::class, 'show'])->name('api.quest-category.show');
 });
 
